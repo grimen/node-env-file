9db31f8c210b203f33ff487974771fa278bbd804
Jonas Grimfelt <grimen@gmail.com>
2015-02-11 21:03:12 -0500

    fix `chai.config` warning

--- test/helper.js
+++ test/helper.js
@@ -3,7 +3,7 @@
 process.env.NODE_ENV = 'test'
 
 module.exports.chai = require('chai')
-module.exports.chai.Assertion.includeStack = true
+module.exports.chai.config.includeStack = true
 
 module.exports.assert = module.exports.chai.assert
 module.exports.expect = module.exports.chai.expect
